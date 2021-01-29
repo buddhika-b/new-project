2223c2885b22a41d3672bf4c629c57dd767975f6
Buddhika <buddhikahalangoda@gmail.com>
2021-01-29 10:05:38 +0530

    java updated

--- cal _java.java
+++ cal _java.java
@@ -2,5 +2,6 @@ public class Hello_World{
   public void static main(String[] args){
       System.out.println("Hello_world!");
       System.out.println("updated");
+      System.out.println("updated2");
   }
 }
